def7249e23b24259010f812b5842f5a8f9df79e4
Piyush Mourya <47456936+mauryapiyush76@users.noreply.github.com>
2021-06-15 17:46:51 +0530

    Update 0007KadanesAlgorithm.java

--- Final450/0007KadanesAlgorithm.java
+++ Final450/0007KadanesAlgorithm.java
@@ -62,5 +62,27 @@ class Solution{
         }
         return maxSoFar;
     }
+	//Method 3 slight opti,ization of method 1
+	static int maxSubArraySum(int a[],int size) 
+{ 
+    
+    int max_so_far = a[0], max_ending_here = 0; 
+
+    for (int i = 0; i < size; i++) 
+    { 
+        max_ending_here = max_ending_here + a[i];
+        if (max_ending_here < 0) 
+            max_ending_here = 0; 
+        
+        /* Do not compare for all
+           elements. Compare only 
+           when max_ending_here > 0 */
+        else if (max_so_far < max_ending_here) 
+            max_so_far = max_ending_here; 
+        
+    } 
+    return max_so_far; 
+} 
+
 }
 

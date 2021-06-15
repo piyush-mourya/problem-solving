a24989b1ab021e98f6b55d7b48a516d34c7fc99e
Piyush Mourya <47456936+mauryapiyush76@users.noreply.github.com>
2021-06-15 17:44:51 +0530

    Update 0007KadanesAlgorithm.java

--- Final450/0007KadanesAlgorithm.java
+++ Final450/0007KadanesAlgorithm.java
@@ -50,5 +50,17 @@ class Solution{
         return max;
     }
     
+	// method 2 Dynamic Programming
+	int maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        int maxSoFar = arr[0];
+        int maxEndingHere = arr[0];
+        for(int i = 1 ; i < n ; i++){
+            maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
 }
 

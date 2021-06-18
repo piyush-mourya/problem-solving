1e62e1041be721b45224292fc64400a2539fd463
Piyush Mourya <47456936+mauryapiyush76@users.noreply.github.com>
2021-06-18 16:58:42 +0530

    Update 0015CountInversions.java

--- Final450/0015CountInversions.java
+++ Final450/0015CountInversions.java
@@ -51,3 +51,63 @@ class Solution
         return invCnt;
     }
 }
+
+// merge sort solution
+class Solution
+{
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
+    //Function to count inversions in the array.
+    static long inversionCount(long arr[], long N)
+    {
+        // Your Code Here
+        long l = 0;
+        long r = N-1;
+        long temp[] = new long[(int)N];
+        return  mergeSort(arr,temp,l,r);
+    }
+    
+    static long mergeSort(long arr[], long temp[], long l, long r){
+        long mid;
+        long invCnt = 0;
+        if(r > l){
+            mid = (l+r)/2;
+            invCnt = mergeSort(arr,temp,l,mid);
+            invCnt += mergeSort(arr,temp,mid+1,r);
+            invCnt += merge(arr,temp,l,mid+1,r);
+        }
+        
+        return invCnt;
+    }
+    
+    static long merge(long arr[], long temp[], long l, long mid, long r){
+        long i,j,k;
+        long invCnt = 0;
+        
+        i = l;
+        j = mid;
+        k = l;
+        
+        while(i <=(mid-1) && j<=r){
+            if(arr[(int)i] <= arr[(int)j]){
+                temp[(int)k++] = arr[(int)i++];
+            } else {
+                temp[(int)k++] = arr[(int)j++];
+                invCnt +=(mid - i);
+            }
+        }
+        
+        while (i <= mid - 1){
+            temp[(int)k++] = arr[(int)i++];
+        }
+        
+        while (j <= r){
+            temp[(int)k++] = arr[(int)j++];
+        }
+        
+        for (i = l; i <= r; i++)
+            arr[(int)i] = temp[(int)i];
+            
+        return invCnt;
+    }
+}

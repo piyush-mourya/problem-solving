56e4984cea8339c0e758428bc9c8cf5be79310cb
Piyush Mourya <47456936+mauryapiyush76@users.noreply.github.com>
2021-12-01 23:00:22 +0530

    Update MaximumSubarray.java

--- leetcode/MaximumSubarray.java
+++ leetcode/MaximumSubarray.java
@@ -1,6 +1,14 @@
 // https://leetcode.com/problems/maximum-subarray/
 
 // Kadane's Algorithm
+/*
+Some more problems based on Kadane Algorithm :)
+121. Best Time to Buy and Sell Stock - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+152. Maximum Product Subarray - https://leetcode.com/problems/maximum-product-subarray/
+918. Maximum Sum Circular Subarray - https://leetcode.com/problems/maximum-sum-circular-subarray/
+978. Longest Turbulent Subarray - https://leetcode.com/problems/longest-turbulent-subarray/
+1749. Maximum Absolute Sum of Any Subarray - https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/
+/*
 class Solution {
     public int maxSubArray(int[] nums) {
         int size = nums.length;

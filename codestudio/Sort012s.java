698af4d61142be661c7083e7462ec336266a9932
Piyush Mourya <47456936+mauryapiyush76@users.noreply.github.com>
2021-12-28 20:11:43 +0530

    Create Sort012s.java

---
+++ codestudio/Sort012s.java
@@ -0,0 +1,26 @@
+// https://www.codingninjas.com/codestudio/problems/sort-0-1-2_631055?source=youtube&campaign=LoveBabbar_Codestudiovideo1&utm_source=youtube&utm_medium=affiliate&utm_campaign=LoveBabbar_Codestudiovideo1
+
+public class Solution 
+{
+    public static void sort012(int[] arr)
+    {
+        int n = arr.length ;
+        int zero = 0, one = 0, two = n - 1 ;
+        while( one <= two ){
+            if(arr[one] == 0){
+                int temp = arr[one];
+                arr[one] = arr[zero];
+                arr[zero]  = temp;
+                zero++;
+                one++;
+            } else if(arr[one] == 2){
+                int temp = arr[two];
+                arr[two] = arr[one];
+                arr[one] = temp;
+                two--;
+            } else {
+               one++;
+            }
+        }
+    }
+}
